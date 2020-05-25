ac6f6c10700ad73900061d91e9f8b84778f340ad
Sam <re711kimo@gmail.com>
2020-05-25 22:12:48 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const exprerss = require('express')
+const mongoose = require('mongoose')
 const app = exprerss()
 const PORT = 3000
+
+mongoose.connect('mongodb://localhost/URL-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello word!')
 })
 
-app.listen(PORT, () =>{
-  console.log(`App is running on http://localhost:3000`)
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
