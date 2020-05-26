47a4c14d93fac58d29f54385a1922a8c7087f548
Sam <re711kimo@gmail.com>
2020-05-26 21:35:36 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const exprerss = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const app = exprerss()
 const PORT = 3000
 
@@ -15,8 +16,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello word!')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
