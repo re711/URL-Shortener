037b60a83095b05b8805ce09ed161bd3f80634b6
Sam <re711kimo@gmail.com>
2020-05-31 13:45:31 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/URL-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost/URL-shortener'
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 
